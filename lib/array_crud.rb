f458524d8241ae993b9cd66efc460cef829f8cab
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-05-18 19:39:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,19 +15,19 @@ array.unshift "#{element}"
 end
 
 def remove_element_from_end_of_array(array)
-array.pop "#{element}"
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-array.shift "#{element}"
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-array[]= "#{element}"
+array[]
 end
 
 def retrieve_first_element_from_array(array)
-array.first= "#{element}"
+array.first
 end
 
 def retrieve_last_element_from_array(array)
